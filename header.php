cc8ff4dd11c40366e906f39c3cc3bf27ed191738
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-05-18 18:33:32 +0200

    Create pages, header and footer

---
+++ header.php
@@ -0,0 +1,62 @@
+<style>
+    * {
+      padding: 0px;
+      margin: 0px;
+      box-sizing: border-box;
+    }
+
+    a {
+      text-decoration: none;
+    }
+
+    body {
+      background-color: #ebebe9;
+      font-family: tahoma;
+    }
+
+    header div {
+      padding: 20px;
+    }
+
+    header a {
+      color: white;
+    }
+
+    header {
+      background-color: #5050cc;
+      display: flex;
+      color: white;
+      justify-content: center;
+      align-items: center;
+    }
+
+    footer {
+      padding: 20px;
+      text-align: center;
+      background-color: #eee;
+    }
+
+    input {
+      margin: 10px;
+      padding: 12px;
+      width: 100%;
+    }
+    
+    textarea {
+      margin: 10px;
+      padding: 12px;
+      width: 100%;
+    }
+
+    button {
+      padding: 10px;
+    }
+
+  </style>
+
+  <header>
+    <div><a href= "index.php"></a>Home</div>
+    <div><a href= "profile.php">Profile</a></div>
+    <div><a href= "login.php">Login</a></div>
+    <div><a href= "signup.php">Signup</a></div>
+  </header>
