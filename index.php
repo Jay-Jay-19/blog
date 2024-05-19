1107eda728b0dcf55ac8e0a000d957561591affc
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-05-19 18:25:14 +0200

    Create post functionality

--- index.php
+++ index.php
@@ -16,6 +16,49 @@
 <body>
 
   <?php require "header.php";?>
+
+    <div style="max-width: 600px; margin:auto;">
+
+      <h3 style="text-align: center;">Timeline</h3>
+      <?php
+        $query = "select * from posts order by id desc limit 10";
+
+        $result = mysqli_query($con, $query);
+      ?>
+
+      <?php if (mysqli_num_rows($result) > 0):?>
+            
+        <?php while ($row = mysqli_fetch_assoc($result)):?>
+
+          <?php
+            $user_id = $row['user_id'];
+            $query = "select username, image from users where id = '$user_id' limit 1";
+            $result2 = mysqli_query($con, $query);
+
+            $user_row = mysqli_fetch_assoc($result2)
+          ?>
+          <div style="background-color: white; display: flex; border: solid thin #aaa; border-radius: 10px; margin-bottom: 10px; margin-top: 10px;">
+            <div style="flex:1; text-align: center;">
+                <img src="<?php echo $user_row['image']?>" style="border-radius: 50%; margin: 10px; width: 75px; height: 75px; object-fit: cover;">
+                <br>
+                <?php echo $user_row['username']?>
+            </div>
+            <div style="flex:8">
+              <?php if (file_exists($row['image'])):?>
+                <div style="">
+                  <img src="<?php echo $row['image']?>" style="width: 100%; height: 200px; object-fit: cover;">
+                </div>
+              <?php endif;?>
+              <div>
+                <div style="color: #888;"><?=date("jS M, Y", strtotime($row['date']))?></div>
+                <?php echo $row['post']?>
+              </div>
+            </div>
+          </div>
+        <?php endwhile;?>
+      <?php endif;?>
+    </div>
+
   <?php require "footer.php";?>
 
 </body>
--- profile.php
+++ profile.php
@@ -4,7 +4,24 @@
 
   check_login();
 
-  if ($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_POST['username']))
+  if ($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_POST['action']) && $_POST['action'] == 'delete')
+  {
+    // delete your profile
+    $id = $_SESSION['info']['id'];
+    $query = "delete from users where id = '$id' limit 1";
+    $result = mysqli_query($con, $query);
+
+    if (file_exists($_SESSION['info']['image'])) {
+      unlink($_SESSION['info']['image']);
+    }
+
+    $query = "delete from posts where user_id = '$id'";
+    $result = mysqli_query($con, $query);
+
+    header("Location: logout.php");
+    die;
+  }
+  elseif ($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_POST['username']))
   {
     // profile edit
     $image_added = false;
@@ -18,6 +35,10 @@
       $image = $folder . $_FILES['image']['name'];
       move_uploaded_file($_FILES['image']['tmp_name'], $image);
 
+      if (file_exists($_SESSION['info']['image'])) {
+        unlink($_SESSION['info']['image']);
+      }
+      
       $image_added = true;
     }
 
@@ -37,7 +58,36 @@
     $query = "select * from users where id = '$id' limit 1";
 
     $result = mysqli_query($con, $query);
-    $row = mysqli_fetch_assoc($result);
+
+    if (mysqli_num_rows($result) > 0) {
+      $_SESSION['info'] = mysqli_fetch_assoc($result);
+    }
+
+    header("Location: profile.php");
+    die;
+  }
+  elseif ($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_POST['post']))
+  {
+    // adding post
+    $image = "";
+    if (!empty($_FILES['image']['name']) && $_FILES['image']['error'] == 0) {
+      // file was uploaded
+      $folder = "uploads/";
+      if (!file_exists($folder))
+      {
+        mkdir($folder,0777,true);
+      }
+      $image = $folder . $_FILES['image']['name'];
+      move_uploaded_file($_FILES['image']['tmp_name'], $image);
+    }
+
+    $post = addslashes($_POST['post']);
+    $user_id = $_SESSION['info']['id'];
+    $date = date('Y-m-d H:i:s');
+
+    $query = "insert into posts (user_id, post, image, date) values ('$user_id', '$post', '$image', '$date')";
+   
+    $result = mysqli_query($con, $query);
 
     header("Location: profile.php");
     die;
@@ -75,6 +125,24 @@
           </a>
         </form>
 
+        <?php elseif (!empty($_GET['action']) && $_GET['action'] === 'delete') :?>
+        
+        <h2 style="text-align: center;">Are you sure you want to delete your profile ?</h2>
+        
+        <div style="margin: auto; max-width: 600px; text-align:center;">
+          <form method="post" style="margin: auto; padding: 10px;">
+
+            <img src="<?php echo $_SESSION['info']['image']?>" style="width: 100px; height: 100px; object-fit:cover; margin: auto; display: block;">
+            <div><?php echo $_SESSION['info']['username']?></div>
+            <div><?php echo $_SESSION['info']['email']?></div>
+            <input type="hidden" name="action" value="delete">
+            <button>Delete</button>
+            <a href="profile.php">
+              <button type="button">Cancel</button>
+            </a>
+          </form>
+        </div>
+
       <?php else:?>
 
         <h2 style="text-align: center;">User profile</h2>
@@ -94,18 +162,74 @@
           <a href="profile.php?action=edit">
             <button>Edit profile</button>
           </a>
+          <a href="profile.php?action=delete">
+            <button>Delete profile</button>
+          </a>
         </div>
         <br>
         <hr>
         <h5>Create a post</h5>
-        <form method="post" style="margin: auto; padding: 10px;">
+        <form method="post" enctype="multipart/form-data" style="margin: auto; padding: 10px;">
 
+          image : <input type="file" name="image"><br>
           <textarea name="post" rows="8"></textarea><br>
 
         <button>Post</button>
 
         </form>
 
+        <hr>
+        <posts>
+          <?php
+            $id = $_SESSION['info']['id'];
+            $query = "select * from posts where user_id = '$id' order by id desc limit 10";
+
+            $result = mysqli_query($con, $query);
+          ?>
+
+          <?php if (mysqli_num_rows($result) > 0):?>
+          
+            <?php while ($row = mysqli_fetch_assoc($result)):?>
+
+              <?php
+
+                $user_id = $row['user_id'];
+                $query = "select username, image from users where id = '$user_id' limit 1";
+                $result2 = mysqli_query($con, $query);
+
+                $user_row = mysqli_fetch_assoc($result2)
+              ?>
+              <div style="background-color: white; display: flex; border: solid thin #aaa; border-radius: 10px; margin-bottom: 10px; margin-top: 10px;">
+                <div style="flex:1; text-align: center;">
+                  <img src="<?php echo $user_row['image']?>" style="border-radius: 50%; margin: 10px; width: 75px; height: 75px; object-fit: cover;">
+                  <br>
+                  <?php echo $user_row['username']?>
+                </div>
+                <div style="flex:8">
+                  <?php if (file_exists($row['image'])):?>
+                    <div style="">
+                      <img src="<?php echo $row['image']?>" style="width: 100%; height: 200px; object-fit: cover;">
+                    </div>
+                  <?php endif;?>
+                  <div>
+                    <div style="color: #888;"><?=date("jS M, Y", strtotime($row['date']))?></div>
+                    <?php echo $row['post']?>
+
+                    <br><br>
+
+                    <a href="profile.php?action=post_edit&id=<?php echo $row['id']?>">
+                      <button>Edit post</button>
+                    </a>
+                    <a href="profile.php?action=post_delete&id=<?php echo $row['id']?>">
+                      <button>Delete post</button>
+                    </a>
+                    <br><br>
+                  </div>
+                </div>
+              </div>
+            <?php endwhile;?>
+          <?php endif;?>
+        </posts>
       <?php endif;?>
 
     </div>
